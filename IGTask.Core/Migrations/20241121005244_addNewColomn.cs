0bafd7c77f73f8440fb505e22b251df4cd8f5cf2
AhmedObeidat <ahmedobee8@gmail.com>
2024-11-21 04:45:05 +0300

    add new colomn

---
+++ IGTask.Core/Migrations/20241121005244_addNewColomn.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IGTask.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class addNewColomn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreateDate",
+                table: "Employees",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Employees",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifyDate",
+                table: "Employees",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreateDate",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "ModifyDate",
+                table: "Employees");
+        }
+    }
+}
---
+++ IGTask.Infra/Service/EmployeesService.cs
@@ -0,0 +1,20 @@
+﻿using IGTask.Core.IRepository;
+using IGTask.Core.IService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IGTask.Infra.Service
+{
+    public class EmployeesService:IEmployeesService
+    {
+        private readonly IEmployeesRepository _repository;
+
+        public EmployeesService(IEmployeesRepository  repository)
+        {
+            _repository = repository;
+        }
+    }
+}
